e374cc880d578c3870053ab3226940ae09a3bba4
Francesco Lattanzio <franz.lattanzio@gmail.com>
2022-12-05 22:10:51 +0100

    Add COPYING file

--- src/dlinkcontrol.c
+++ src/dlinkcontrol.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Francesco Lattanzio
+ * Copyright (c) 2022 Francesco Lattanzio
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
